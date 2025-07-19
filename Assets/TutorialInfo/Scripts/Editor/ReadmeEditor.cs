ebcfbf2c59e725e7410c0baba7f4988a4bcbe237
CatFishe <23521176@gm.uit.edu.vn>
2025-07-19 14:58:19 +0700

    test

--- Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
+++ Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
@@ -13,7 +13,7 @@ public class ReadmeEditor : Editor {
 	static string kShowedReadmeSessionStateName = "ReadmeEditor.showedReadme";
 	
 	static float kSpace = 16f;
-	
+	//lmao
 	static ReadmeEditor()
 	{
 		EditorApplication.delayCall += SelectReadmeAutomatically;
